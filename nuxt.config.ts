6f4797e2dadabcb7fe5b37294a7d96180f8f8ebb
jfsenechal <jfsenechal@gmail.com>
2024-10-14 16:50:40 +0200

    remove debug

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -21,7 +21,6 @@ export default defineNuxtConfig({
         },
         pageTransition: {name: 'page', mode: 'out-in'}
     },
-    debug:true,
     runtimeConfig: {
         public: {
             API_URL_SERVER: process.env.API_URL_SERVER,
