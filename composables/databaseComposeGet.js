645ca1531c0e38134e397abdf428d0d4ade418c3
jfsenechal <jfsenechal@gmail.com>
2024-10-16 13:45:44 +0200

    try better response error

--- composables/databaseComposeGet.js
+++ composables/databaseComposeGet.js
@@ -6,6 +6,18 @@ export default (id = null) => {
         useFetch(`${config.public.API_URL}/notion-php/getDatabase.php`, {
             query: {id: id, refresh: refreshing},
             key: 'database-' + id,
+            onResponseError(context) {
+                const response = context.response
+                const error = context.error
+                console.log("error message " + error?.message)
+                console.log("error: " + JSON.stringify(context))
+                status.value = "error"
+                error.value = error?.message
+            },
+            onResponse(context) {
+                status.value = "success"
+                error.value = null
+            }
         })
     return {
         status,
--- composables/pageComposeGet.js
+++ composables/pageComposeGet.js
@@ -3,11 +3,23 @@ import {isRefreshing} from "~/composables/cacheUtil.js";
 export default (pageId = null) => {
     const refreshing = isRefreshing()
     const config = useRuntimeConfig()
-    console.log(`fetch ${config.public.API_URL}/notion-php/getPage.php?page_id=' + ${pageId}`)
+    console.log(`fetch ${config.public.API_URL}/notion-php/getPage.php?page_id=${pageId}`)
     const {status, data, error} =
         useFetch(`${config.public.API_URL}/notion-php/getPage.php`, {
             query: {page_id: pageId, refresh: refreshing},
             key: 'page-' + pageId,
+            onResponseError(context) {
+                const response = context.response
+                const error = context.error
+                console.log("error message " + error?.message)
+                console.log("error: " + JSON.stringify(context))
+                status.value = "error"
+                error.value = error?.message
+            },
+            onResponse(context) {
+                status.value = "success"
+                error.value = null
+            }
         })
 
     console.log("status: " + status.value, "error " + error.value, "data " + data.value)
