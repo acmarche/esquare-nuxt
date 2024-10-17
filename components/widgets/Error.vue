12af92069c496c336cccddfa6d775216d446bd7c
jfsenechal <jfsenechal@gmail.com>
2024-10-17 10:31:38 +0200

    better error show

--- components/widgets/Error.vue
+++ components/widgets/Error.vue
@@ -3,14 +3,16 @@ import {IconBug} from '@tabler/icons-vue';
 
 const {error} = defineProps<{
   error: {
-    type: String, required: false, default: null
+    type: Object,
+    required: false,
+    default: null
   },
 }>()
 </script>
 
 <template>
-  <div class="text-red-600 p-4 m-4">
-    <IconBug class="h-8 w-8 flex-shrink-0" aria-hidden="true"/>
-    Error: {{ error }}
+  <div class="flex flex-row items-center text-red-600 p-4 m-4">
+    <IconBug class="h-8 w-8 flex-shrink-0 mr-3" aria-hidden="true"/>
+    <span>{{ error?.data?.error?.message ?? error.message }}</span>
   </div>
 </template>
--- composables/databaseActivitiesComposeGet.js
+++ composables/databaseActivitiesComposeGet.js
@@ -2,7 +2,7 @@ export default (id = null) => {
     const config = useRuntimeConfig()
     const refreshing = isRefreshing()
     console.log(`${config.public.API_URL}/notion-php/activitiesDatabase.php?id=${id}`)
-    const {status, data, error, refresh} =
+    const {status, data, error} =
         useFetch(`${config.public.API_URL}/notion-php/activitiesDatabase.php`, {
             query: {id: id, refresh: refreshing},
             key: 'database-activities-' + id,
