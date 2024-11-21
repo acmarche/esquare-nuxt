07b2a8cbb77b4ceabc13f3c386910d207222dcd4
jfsenechal <jfsenechal@gmail.com>
2024-11-21 13:23:46 +0100

    new template draft

--- components/databases/views/MachineItem.vue
+++ components/databases/views/MachineItem.vue
@@ -14,28 +14,47 @@ const names = computed(() => {
 const images = computed(() => {
   return properties.value['Image']
 })
+const icones = computed(() => {
+  return properties.value['Icone']
+})
 const reserver = computed(() => {
   return properties.value['Réserver']
 })
 const descriptions = computed(() => {
   return properties.value.Description
 })
+const modalOpenSelected = ref('')
 </script>
 <template>
-  <div>
-    <div class="flex flex-col gap-3">
-    <span class="text-2xl">
-      <BlockRichText :property="names"/>
-    </span>
-      <span>
+  <div class="flex flex-col items-center justify-center">
+    <div  class="flex flex-col items-center justify-center rounded-lg shadow-xl p-8">
+      <div v-for="image in icones['files']" v-if="icones['files'].length > 0">
+        <BlockImage :property="image" width="150"/>
+      </div>
+      <img width="150" src="https://e-square.marche.be/wp-content/uploads/2020/03/cnc_mekanika-1-401x450.png" alt="icon"
+           v-else>
+      <h3 class="text-2xl mt-2">
+        <BlockRichText :property="names"/>
+      </h3>
+      <span class="my-2">
       <BlockRichText :property="descriptions"/>
     </span>
+      <button type="button" @click="modalOpenSelected=machine.id"
+                  class="relative inline-flex w-0 flex-1 items-center justify-center gap-x-3 rounded-br-lg border border-transparent py-4 text-sm font-semibold text-gray-900">
+            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none"
+                 stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
+                 class="icon icon-tabler icons-tabler-outline icon-tabler-meeple">
+              <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+              <path
+                  d="M9 20h-5a1 1 0 0 1 -1 -1c0 -2 3.378 -4.907 4 -6c-1 0 -4 -.5 -4 -2c0 -2 4 -3.5 6 -4c0 -1.5 .5 -4 3 -4s3 2.5 3 4c2 .5 6 2 6 4c0 1.5 -3 2 -4 2c.622 1.093 4 4 4 6a1 1 0 0 1 -1 1h-5c-1 0 -2 -4 -3 -4s-2 4 -3 4z"/>
+            </svg>
+            Détails de la machine
+          </button>
+
     </div>
-    <div v-for="image in images['files']">
-      <BlockImage :property="image"/>
-    </div>
-    <div v-if="reserver.url" class="">
-      <BlockUrl :property="reserver" name="Réserver"/>
-    </div>
+
+    <DatabasesViewsMachineModal :machine v-model:modal-open-selected="modalOpenSelected"
+                                :key="machine.id"
+                                v-show="modalOpenSelected === machine.id"/>
   </div>
 </template>
---
+++ components/databases/views/MachineModal.vue
@@ -0,0 +1,93 @@
+<script setup>
+const {machine} = defineProps({
+  machine: {
+    type: Object,
+    required: true
+  },
+})
+const modalOpenSelected = defineModel('modalOpenSelected')
+const properties = computed(() => {
+  return machine.properties
+})
+const image = computed(() => {
+  return 'https://images.unsplash.com/photo-1532417344469-368f9ae6d187?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=4&w=256&h=256&q=60'
+})
+</script>
+<template>
+  <div class="relative z-10" aria-labelledby="modal-title" role="dialog" aria-modal="true">
+    <!--
+      Background backdrop, show/hide based on modal state.
+
+      Entering: "ease-out duration-300"
+        From: "opacity-0"
+        To: "opacity-100"
+      Leaving: "ease-in duration-200"
+        From: "opacity-100"
+        To: "opacity-0"
+    -->
+    <transition
+        enter-active-class="ease-out duration-300"
+        enter-from-class="opacity-0"
+        enter-to-class="opacity-100"
+        leave-active-class="ease-in duration-200"
+        leave-from-class="opacity-100"
+        leave-to-class="opacity-0"
+    >
+      <div class="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity" aria-hidden="true"></div>
+    </transition>
+    <div class="fixed inset-0 z-10 w-screen overflow-y-auto">
+      <div class="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0">
+
+        <transition
+            enter-active-class="ease-out duration-300"
+            enter-from-class="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+            enter-to-class="opacity-100 translate-y-0 sm:scale-100"
+            leave-active-class="ease-in duration-200"
+            leave-from-class="opacity-100 translate-y-0 sm:scale-100"
+            leave-to-class="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95">
+          <!--
+            Modal panel, show/hide based on modal state.
+
+            Entering: "ease-out duration-300"
+              From: "opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+              To: "opacity-100 translate-y-0 sm:scale-100"
+            Leaving: "ease-in duration-200"
+              From: "opacity-100 translate-y-0 sm:scale-100"
+              To: "opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+          -->
+          <div
+              class="relative transform overflow-hidden rounded-lg bg-white px-4 pb-4 pt-5 text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-lg sm:p-6">
+            <div class="flex flex-col sm:items-start">
+              <div class="flex flex-col items-center mx-auto mb-3">
+                <NuxtImg class="mx-auto h-32 w-32 flex-shrink-0 rounded-full" :src="image" alt=""/>
+                <div class="mt-3 text-center sm:ml-4 sm:mt-0 sm:text-left">
+                  <h3 class="text-2xl font-semibold leading-6 text-gray-900" id="modal-title">
+                    Nom machine
+                  </h3>
+                </div>
+                <span
+                    class="inline-flex items-center rounded-full bg-green-50 px-2 py-1 font-medium text-green-700 ring-1 ring-inset ring-green-600/20">
+                  xxx
+                </span>
+              </div>
+              <div class="mt-2">
+                <p class="text-sm text-gray-500">
+                  xxx
+                </p>
+              </div>
+
+
+            </div>
+            <div class="mt-5 sm:mt-4 sm:flex sm:flex-row-reverse">
+              <button type="button"
+                      @click="modalOpenSelected=''"
+                      class="mt-3 inline-flex w-full justify-center rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50 sm:mt-0 sm:w-auto">
+                Fermer
+              </button>
+            </div>
+          </div>
+        </transition>
+      </div>
+    </div>
+  </div>
+</template>
