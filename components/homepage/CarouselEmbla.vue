b05a0b9479d92a93d12f1921cf0c0c71216a764a
jfsenechal <jfsenechal@gmail.com>
2024-11-27 10:23:08 +0100

    test carousel

--- components/homepage/CarouselEmbla.vue
+++ components/homepage/CarouselEmbla.vue
@@ -27,20 +27,11 @@ onMounted(() => {
 <template>
   <WidgetsLoader v-if="status === 'pending'"/>
   <WidgetsError v-else-if="error" :error/>
-  <div class="flex flex-row w-full"
+  <div class="relative bg-gray-200 flex flex-row justify-end w-full"
+       style="background-image: url('/images/batimentaerien.jpg'); background-position: left -25% center; background-repeat: no-repeat;"
        v-else>
-    <div class="bg-gray-200 bg-cover bg-center flex flex-col items-center justify-center relative basis-2/3 h-[25rem]"
-         style="background-image: url('/images/batimentaerien.png'); background-position: center 50%;"></div>
-    <div class="overflow-hid4den basis-1/3">
-      <div class="embla" ref="emblaRef">
-        <div class="embla__container">
-          <div class="embla__slide"
-               v-for="page in events"
-               :key="page.id">
-            <NuxtImg :src="image(page)" class="object-cover aspect-square w-25[rem] md:h-[25rem] " alt="img"/>
-          </div>
-        </div>
-      </div>
+    <div class="flex items-center justify-center relative h-[35rem]">
+       <NuxtImg class="object-cover aspect-square w-25[rem] md:h-[35rem]" src="https://prod-files-secure.s3.us-west-2.amazonaws.com/342f974b-11f0-497c-a847-4ef08f933cb7/19b3a04b-1c5c-49eb-bcb5-65a577a3be00/Pitch_x_Network_%282%29.jpg?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Content-Sha256=UNSIGNED-PAYLOAD&X-Amz-Credential=AKIAT73L2G45FSPPWI6X%2F20241127%2Fus-west-2%2Fs3%2Faws4_request&X-Amz-Date=20241127T090052Z&X-Amz-Expires=3600&X-Amz-Signature=02f7c92b243f7460facb38f5d1f6fd66888622a2fa05c98d72cfa188029035e5&X-Amz-SignedHeaders=host&x-id=GetObject" alt="img"/>
     </div>
   </div>
 </template>
