e10e2011f4a91fc3479e3b3a2760a661345d93f2
jfsenechal <jfsenechal@gmail.com>
2024-12-18 13:17:12 +0100

    try carousel

--- components/homepage/CarouselEmbla.vue
+++ components/homepage/CarouselEmbla.vue
@@ -25,14 +25,12 @@ onMounted(() => {
 })
 </script>
 <template>
-  <WidgetsLoader v-if="status === 'pending'"/>
-  <WidgetsError v-else-if="error" :error/>
   <section class=" grid bannerSection h-[40rem] w-full"
-           style="background-image: url('/images/batimentaerien.jpg'); background-position: left -25% center; background-repeat: no-repeat;"
-           v-else>
+           style="background-image: url('/images/batimentaerien-cut.jpg'); background-position: left -45% center; background-repeat: no-repeat;"
+  >
     <div class="relative flex items-center">
-       <div
-        class="absolute inset-0 bg-gradient-to-b from-transparent via-black/60 to-black/90 opacity-50 transition group-hover:opacity-100"></div>
+      <div
+          class="absolute inset-0 bg-gradient-to-r from-transparent  to-black/90  transition group-hover:opacity-100"></div>
       <div class=" text-white text-3xl font-bold mt-6 prose z-10 font-Oswald px-2">
         "C’est par la rencontre et l’échange de points de vue que les idées fleurissent ;
         et c’est par l’échange de capacités et de compétences qu’elles se réalisent.
@@ -40,15 +38,17 @@ onMounted(() => {
       </div>
     </div>
     <div>
-<div class="embla" ref="emblaRef">
-      <div class="embla__container">
-        <div class="embla__slide"
-             v-for="page in events"
-             :key="page.id">
-          <NuxtImg :src="image(page)" class="object-cover aspect-square w-40[rem] md:h-[40rem] " alt="img"/>
+      <WidgetsLoader v-if="status === 'pending'"/>
+      <WidgetsError v-else-if="error" :error/>
+      <div class="embla" ref="emblaRef" v-else>
+        <div class="embla__container">
+          <div class="embla__slide"
+               v-for="page in events"
+               :key="page.id">
+            <NuxtImg :src="image(page)" class="object-cover aspect-square w-40[rem] md:h-[40rem] " alt="img"/>
+          </div>
         </div>
       </div>
-    </div>
     </div>
   </section>
 </template>
