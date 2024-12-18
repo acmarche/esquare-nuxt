7368600b8e469e1b068530fa5579df0ca0dc217d
jfsenechal <jfsenechal@gmail.com>
2024-12-18 13:33:35 +0100

    new button fill out

--- components/block/RichText.vue
+++ components/block/RichText.vue
@@ -24,7 +24,7 @@ const color = ref(null)
 
 function getColor(text) {
   color.value = extractColor(text)
-  if (color.value != 'false') {
+  if (color.value != false) {
     return color
   } else {
     return null
@@ -42,7 +42,7 @@ function isFillOut(text) {
 <template>
   <template v-for="text in texts" :key="text" v-if="texts && texts.length > 0">
     <component :is="hasLink(text) ? 'a' : 'span'"
-               :class="[isFillOut(text) ? 'buttonFillOut':'',isBold(text) ? 'font-semibold' : '',isItalic(text) ? 'italic' : '', underline(text) ? 'underline':'',strikethrough(text) ? 'line-through':'']"
+               :class="['inline-block',isFillOut(text) ? 'buttonFillOut':'',isBold(text) ? 'font-semibold' : '',isItalic(text) ? 'italic' : '', underline(text) ? 'underline':'',strikethrough(text) ? 'line-through':'']"
                :style="[getColor(text) ? 'color:' + color : '']"
                :target="hasLink(text) ? '_blank' : '_self'"
                :href="text.text?.link?.url"
@@ -54,7 +54,7 @@ function isFillOut(text) {
 <style>
 .buttonFillOut {
   @media (min-width: 768px) {
-    @apply block text-left border border-esquare-yellow font-medium rounded-lg m-2 px-6 py-2 shadow-md hover:border-esquare-blue hover:text-esquare-black hover:shadow-lg transition duration-200
-  }
+    @apply mx-1 rounded-md bg-esquare-yellow px-3.5 py-2.5 text-center font-semibold text-white hover:text-white text-lg shadow-sm hover:bg-esquare-blue focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 transition-all duration-500
+   }
 }
 </style>
