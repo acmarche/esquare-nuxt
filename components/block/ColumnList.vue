01b74e7c00444418f6b9264089d3261907634fc5
jfsenechal <jfsenechal@gmail.com>
2024-12-18 13:33:20 +0100

    change column mobile/pc

--- components/block/ColumnList.vue
+++ components/block/ColumnList.vue
@@ -7,7 +7,7 @@ defineProps({
 })
 </script>
 <template>
-  <section class="flex flex-row items-center gap-3 justify-between w-full">
+  <section class="flex flex-col md:flex-row items-center gap-1 md:gap-3 justify-between w-full">
     <div v-for="column in property.columns" :key="column.id">
         <BlockColumn :column="column"/>
     </div>
